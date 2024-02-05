ae2942f4e56b7e93d49a4d85ae9efce9b186ae75
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2024-02-05 19:44:15 +0000

    Initial commit

---
+++ src/main/java/net/tiagofar78/prisonescape/App.java
@@ -0,0 +1,13 @@
+package net.tiagofar78.prisonescape;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
