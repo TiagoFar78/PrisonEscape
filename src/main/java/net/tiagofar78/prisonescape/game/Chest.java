ddb07931c952adcad5ded7232f443c4ad203fc49
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2024-02-27 23:58:11 +0000

    Started implementing Chest

--- src/main/java/net/tiagofar78/prisonescape/game/Chest.java
+++ src/main/java/net/tiagofar78/prisonescape/game/Chest.java
@@ -1,5 +1,31 @@
 package net.tiagofar78.prisonescape.game;
 
+import java.util.List;
+
+import org.bukkit.inventory.ItemStack;
+
 public class Chest {
+	
+	private int _size;
+	private List<ItemStack> _contents;
+	private List<ItemProbability> _itemsProbability;
+	
+	public Chest(int size, List<ItemProbability> itemsProbability) {
+		this._size = size;
+		this._itemsProbability = itemsProbability;
+		
+		reload();
+	}
+	
+	public void reload() {
+		_contents.clear();
+		
+		
+	}
+	
+	
+	public void removeItem() {
+		
+	}
 
 }
---
+++ src/main/java/net/tiagofar78/prisonescape/game/ItemProbability.java
@@ -0,0 +1,23 @@
+package net.tiagofar78.prisonescape.game;
+
+import org.bukkit.inventory.ItemStack;
+
+public class ItemProbability {
+	
+	private ItemStack _item;
+	private double _probability;
+	
+	public ItemProbability(ItemStack item, double probability) {
+		_item = item;
+		_probability = probability;
+	}
+	
+	public ItemStack getItem() {
+		return _item;
+	}
+	
+	public double getProbability() {
+		return _probability;
+	}
+
+}
