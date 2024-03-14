0c11bd472011eb6d31c96e65d7e4eade86d9451d
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2024-03-14 21:27:50 +0000

    Implemented TeleportManager

---
+++ src/main/java/net/tiagofar78/prisonescape/managers/TeleportManager.java
@@ -0,0 +1,28 @@
+package net.tiagofar78.prisonescape.managers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import net.tiagofar78.prisonescape.game.PrisonEscapePlayer;
+import net.tiagofar78.prisonescape.game.prisonbuilding.PrisonEscapeLocation;
+
+public class TeleportManager {
+	
+	private static final double CENTER_OF_BLOCK = 0.5;
+	
+	private static final World WORLD = Bukkit.getWorld(ConfigManager.getInstance().getWorldName());
+	
+	public static void teleport(PrisonEscapePlayer player, PrisonEscapeLocation loc) {
+		Player bukkitPlayer = Bukkit.getPlayer(player.getName());
+		if (bukkitPlayer == null) {
+			return;
+		}
+		
+		Location bukkitLocation = new Location(WORLD, loc.getX() + CENTER_OF_BLOCK, loc.getY(), loc.getZ() + CENTER_OF_BLOCK);
+		
+		bukkitPlayer.teleport(bukkitLocation);
+	}
+
+}
